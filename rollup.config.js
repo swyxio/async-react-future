d4de53651e144cf5c41c4231feea9384f18180a8
tsiq-swyx <shawn.wang@twosigmaiq.com>
2018-05-10 15:06:21 -0400

    it worked, now to try exporting components separately

--- rollup.config.js
+++ rollup.config.js
@@ -1,34 +1,63 @@
-import babel from "rollup-plugin-babel";
-import commonjs from "rollup-plugin-commonjs";
-import external from "rollup-plugin-peer-deps-external";
-import postcss from "rollup-plugin-postcss";
-import resolve from "rollup-plugin-node-resolve";
-import url from "rollup-plugin-url";
+import babel from 'rollup-plugin-babel';
+import commonjs from 'rollup-plugin-commonjs';
+import external from 'rollup-plugin-peer-deps-external';
+import postcss from 'rollup-plugin-postcss';
+import resolve from 'rollup-plugin-node-resolve';
+import url from 'rollup-plugin-url';
 
-import pkg from "./package.json";
+import pkg from './package.json';
 
-export default {
-  input: "src/index.js",
-  output: [
-    {
-      file: pkg.main,
-      format: "cjs"
-    },
-    {
-      file: pkg.module,
-      format: "es"
-    }
-  ],
-  plugins: [
-    external(),
-    postcss({
-      modules: true
-    }),
-    url(),
-    babel({
-      exclude: "node_modules/**"
-    }),
-    resolve(),
-    commonjs()
-  ]
-};
+export default [
+  {
+    input: 'src/index.js',
+    output: [
+      {
+        file: pkg.main,
+        format: 'cjs'
+      },
+      {
+        file: pkg.module,
+        format: 'es'
+      }
+    ],
+    plugins: [
+      external(),
+      postcss({
+        modules: true
+      }),
+      url(),
+      babel({
+        runtimeHelpers: true,
+        exclude: 'node_modules/**'
+      }),
+      resolve(),
+      commonjs()
+    ]
+  },
+  {
+    input: 'src/components/index.js',
+    output: [
+      {
+        file: pkg.componentdir.main,
+        format: 'cjs'
+      },
+      {
+        file: pkg.componentdir.module,
+        format: 'es'
+      }
+    ],
+    plugins: [
+      external(),
+      postcss({
+        modules: true
+      }),
+      url(),
+      babel({
+        runtimeHelpers: true,
+        exclude: 'node_modules/**'
+      }),
+      resolve(),
+      commonjs()
+    ]
+  }
+];
--- src/components/index.js
+++ src/components/index.js
@@ -1,2 +1,4 @@
 import './index.css';
 export * from './Spinner.js';
+export * from './MoviePage.js';
+export * from './MovieListPage.js';
